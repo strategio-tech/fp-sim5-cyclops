b3d98fa0da6ccb450c3857aafe04767f83642133
Nicolo` <nicolo.frison97@gmail.com>
2022-11-21 16:53:50 -0500

    Add simple security for pages where the veterinarians only should access with simple credentials inside the application.properties

---
+++ src/main/java/org/springframework/samples/petclinic/config/ProjectSecurityConfig.java
@@ -0,0 +1,34 @@
+package org.springframework.samples.petclinic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+			.antMatchers("/owners/**","/pets/**").authenticated()
+			.anyRequest().permitAll()
+			.and().formLogin()
+			.and().httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
